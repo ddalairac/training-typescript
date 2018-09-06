ef07a45e59e295b05946736b5510f6590e1e4073
diego <stereotipoarg@gmail.com>
2018-09-06 15:17:59 -0300

    readme

--- 00-setup/main.ts
+++ 00-setup/main.ts
@@ -1,4 +1,4 @@
-class Hello {
+/* class Hello {
 	constructor(public messaje: string) { }
 	getMessage() {
 			return "<h1>" + this.messaje + "</h1>";
@@ -8,3 +8,34 @@ class Hello {
 var hello = new Hello("hello world?");
 
 document.body.innerHTML = hello.getMessage();
+
+ */
+
+
+interface Heroes {
+		name: string;
+		weapon: string;
+		posicion: number;
+} 
+let heroes = [
+	{
+			name: "Luke Skywalker",
+			weapon: "Saber laser ",
+			posicion: 4
+	},
+	{
+			name: "Aragon",
+			weapon: "Andúril",
+			posicion: 1
+	},
+	{
+			name: "Marty McFly",
+			weapon: "Skate board",
+			posicion: 2
+	},
+	{
+			name: "Guybrush Threepwood",
+			weapon: "none",
+			posicion: 3
+	}
+]
---
+++ README.md
@@ -0,0 +1,7 @@
+## Typescript Training
+
+En el repositorio pueden encontrarse los ejemplos del curso de Typescript
+
+
+El curso puede verse en este PPT compartido.
+https://docs.google.com/presentation/d/1_TuRqsbN2u89z_lZYgwX8amSCGfvYZaBLX_4fPnAFHE/edit?usp=sharing
